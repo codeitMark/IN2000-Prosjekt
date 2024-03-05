c4d577d81282dc21054aa9b88e9e5237349bd9cf
mkpoblet <mkpoblet@uio.no>
2024-03-05 13:10:31 +0100

    Add dependencies nav, ktor, viewmodel and coil

--- project/app/build.gradle.kts
+++ project/app/build.gradle.kts
@@ -51,6 +51,11 @@ android {
 
 dependencies {
 
+    dependencies {
+        val nav_version = "2.7.6"
+        implementation("androidx.navigation:navigation-compose:$nav_version")
+    }
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -62,8 +67,17 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.01.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    val ktor_version = "2.3.6"
+    implementation("io.ktor:ktor-client-android:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-gson:$ktor_version")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
+    implementation("io.coil-kt:coil-compose:2.5.0")
 }
