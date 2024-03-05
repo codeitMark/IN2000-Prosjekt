b6638251836338a1581e24901b105aafbb4221f7
mkpoblet <mkpoblet@uio.no>
2024-03-05 13:11:11 +0100

    Update nav dependency 2.7.6 -> 2.7.7

--- project/app/build.gradle.kts
+++ project/app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 dependencies {
 
     dependencies {
-        val nav_version = "2.7.6"
+        val nav_version = "2.7.7"
         implementation("androidx.navigation:navigation-compose:$nav_version")
     }
 
