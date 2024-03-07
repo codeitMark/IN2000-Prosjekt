7ce7f33e27c3ca5483c5bd73dc9ac9c3308efe90
Hassan Qadeer <hassanqa@ulrik.uio.no>
2024-03-07 12:20:45 +0100

    Name change in Greeting

--- project/app/src/main/java/no/uio/ifi/in2000/project/MainActivity.kt
+++ project/app/src/main/java/no/uio/ifi/in2000/project/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Marte")
+                    Greeting("Gruppe 38")
                 }
             }
         }
