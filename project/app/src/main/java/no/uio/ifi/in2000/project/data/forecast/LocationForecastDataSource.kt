919f50fd718526c8612eb953312bd5618db360bf
mkpoblet <mkpoblet@uio.no>
2024-03-19 11:43:52 +0100

    Remove second httpclient

--- project/app/src/main/java/no/uio/ifi/in2000/project/data/forecast/LocationForecastDataSource.kt
+++ project/app/src/main/java/no/uio/ifi/in2000/project/data/forecast/LocationForecastDataSource.kt
@@ -14,19 +14,17 @@ import no.uio.ifi.in2000.project.model.forecast.LocationForecastResponse
 
 
 data class LocationForecastDataSource(private val path: String = "https://gw-uio.intark.uh-it.no/in2000/") {
-    private val client = HttpClient(){
+    private val client = HttpClient(CIO){
         install(ContentNegotiation){
             gson()
+        }
+        defaultRequest { 
+            url(path)
+            header("X-Gravitee-Api-Key", "2da3279c-ee4c-4d21-955e-d13822ff578c")
         }
     }
 
     suspend fun getWeather(): LocationForecastResponse{
-        val klienten = HttpClient(CIO){
-            defaultRequest {
-                url(path)
-                header("X-Gravitee-Api-Key", "2da3279c-ee4c-4d21-955e-d13822ff578c")
-            }
-        }
         val httpResponse = client.get("weatherapi/locationforecast/2.0/compact?lat=60&lon=11")
         Log.i("LocationForecastDataSource", "response ${httpResponse.status.value}")
         //val response = httpResponse.body<LocationForecastResponse>()
