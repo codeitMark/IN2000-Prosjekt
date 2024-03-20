7d832cbc1cbcdd50b2f38e426b6d16fec64f9ba6
ibenso <ibenso@uio.no>
2024-03-20 14:06:54 +0100

    Added showBackground and showSystemUi to preview

--- project/app/src/main/java/no/uio/ifi/in2000/project/ui/home/HomeScreen.kt
+++ project/app/src/main/java/no/uio/ifi/in2000/project/ui/home/HomeScreen.kt
@@ -5,15 +5,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,7 +21,7 @@ import coil.compose.AsyncImage
 import coil.decode.SvgDecoder
 import coil.request.ImageRequest
 
-@Preview
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun HomeScreen(vm: HomeViewModel = viewModel()){
     val scrollState = rememberScrollState()
