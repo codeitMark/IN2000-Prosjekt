ac84466bab45173058585ea682efae5c84079e08
mkpoblet <mkpoblet@uio.no>
2024-03-20 20:16:21 +0100

    Update MetAlertsRepository val to parameter

--- project/app/src/main/java/no/uio/ifi/in2000/project/data/alerts/MetAlertsRepository.kt
+++ project/app/src/main/java/no/uio/ifi/in2000/project/data/alerts/MetAlertsRepository.kt
@@ -2,8 +2,7 @@ package no.uio.ifi.in2000.project.data.alerts
 
 import no.uio.ifi.in2000.project.model.alerts.Features
 
-class MetAlertsRepository {
-    private val metAlertsSource = MetAlertsDataSource()
+class MetAlertsRepository(private val metAlertsSource: MetAlertsDataSource) {
 
     suspend fun fetchAlerts(lat: Double, lon: Double, lang: String): Features {
         return metAlertsSource.getAlerts(lat, lon, lang)
