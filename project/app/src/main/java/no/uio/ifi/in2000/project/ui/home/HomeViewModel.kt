f1f6ffa10b957e10875a568dca7de6040463adb3
mkpoblet <mkpoblet@uio.no>
2024-03-21 16:36:19 +0100

    Add metAlertsRepository to HomeViewModel. Update responseStatus to update HomeScreen when changed.

--- project/app/src/main/java/no/uio/ifi/in2000/project/ui/home/HomeViewModel.kt
+++ project/app/src/main/java/no/uio/ifi/in2000/project/ui/home/HomeViewModel.kt
@@ -8,8 +8,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import no.uio.ifi.in2000.project.data.alerts.MetAlertsDataSource
+import no.uio.ifi.in2000.project.data.alerts.MetAlertsRepository
 import no.uio.ifi.in2000.project.data.forecast.LocationForecastDataSource
 import no.uio.ifi.in2000.project.data.forecast.LocationForecastRepository
+import no.uio.ifi.in2000.project.model.alerts.MetAlertsResponse
 import no.uio.ifi.in2000.project.model.forecast.Data
 import no.uio.ifi.in2000.project.model.forecast.Geometry
 import no.uio.ifi.in2000.project.model.forecast.Instant
@@ -26,31 +29,40 @@ import no.uio.ifi.in2000.project.model.forecast.Units
 //data class weatherUiState(val data: LocationForecastResponse)
 
 class HomeViewModel : ViewModel(){
-    private val rep = LocationForecastRepository(LocationForecastDataSource())
-    var responseStatus = false
+    private val locationForecastRep = LocationForecastRepository(LocationForecastDataSource())
+    private val metAlertsRep = MetAlertsRepository(MetAlertsDataSource())
+    var responseStatus by mutableStateOf(false) //made mutableStateOf so HomeScreen updates if responseStatus changes. Without this it will NOT update since the combination of weatherData and alertsData in init takes too long.
 
     //var weatherUiState by mutableStateOf(weatherUiState) //the same as weatherData
 
 
-    private val emptyResponse = LocationForecastResponse("empty", Geometry("empty", emptyList()), Properties(
+    private val locationForecastEmptyResponse = LocationForecastResponse("empty", Geometry("empty", emptyList()), Properties(
         Meta("empty", Units("empty", "empty", "empty", "empty", "empty", "empty", "empty")),
         listOf(TimeSeries("empty", Data(Instant(Instant_Details(0.toFloat(), 0.toFloat(), 0.toFloat(), 0.toFloat(), 0.toFloat(), 0.toFloat())), NextHours(
             Summary("empty"), NextHours_Details(0.toFloat())
         ), NextHours(Summary("empty"), NextHours_Details(0.toFloat())), NextHours(Summary("empty"), NextHours_Details(0.toFloat())))))
     )) //Used as a placeholder for weatherData's content (mutableStateOf)
-    var weatherData by mutableStateOf(emptyResponse)
+
+    private val metAlertsEmptyResponse = MetAlertsResponse(emptyList(), "empty", "empty", "empty") //Used as a placeholder for alertsData's content (mutableStateOf)
+
+    var weatherData by mutableStateOf(locationForecastEmptyResponse)
+        private set
+
+    var alertsData by mutableStateOf(metAlertsEmptyResponse)
         private set
 
     // bare midlertidige hardkodede koordinater for MVP-en
     val lat = 58.7753
     val lon = 5.90566
+    val lang = "no"
 
     init {
         viewModelScope.launch(Dispatchers.IO){
-            weatherData = rep.fetchWeather(lat, lon)
+            weatherData = locationForecastRep.fetchWeather(lat, lon)
+            alertsData = metAlertsRep.fetchAlerts(lat, lon, lang)
             responseStatus = true
             //Log.i("HOMEVIEWMODEL INIT", "Initiated.")
-            //weatherUiState = weatherUiState.copy(weather = weather) //same functionality as weatherData = rep.fetchWeather(lat, lon)
+            //weatherUiState = weatherUiState.copy(weather = weather) //same functionality as weatherData = locationForecastRep.fetchWeather(lat, lon)
         }
     }
 }
