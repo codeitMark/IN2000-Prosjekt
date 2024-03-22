cd1c2db42ba530e674eb49f3c2919587554a4f82
mkpoblet <mkpoblet@uio.no>
2024-03-22 20:50:31 +0100

    Add explanation to if-else null to LocationForecastDataSource

--- project/app/src/main/java/no/uio/ifi/in2000/project/data/forecast/LocationForecastDataSource.kt
+++ project/app/src/main/java/no/uio/ifi/in2000/project/data/forecast/LocationForecastDataSource.kt
@@ -47,7 +47,7 @@ data class LocationForecastDataSource(private val path: String = "https://gw-uio
                 authorized = true
                 httpResponse.body<LocationForecastResponse>() //Samme som det kommentert over
             } else{
-                null
+                null //Innholdet blir null uansett, bare at man får LocationForecastResponse objekt med parameterne null. Enklere å bare gjøre det til null tidlig nå, så slipper vi å lage en if-sjekk senere for om det det som er inni er null.
             }
         } catch (e: Exception){
             //No internet connection because other errors will return an empty list of the respective Response data class.
