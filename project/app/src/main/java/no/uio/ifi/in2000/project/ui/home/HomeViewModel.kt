2920e5ed88226ffb5bce0f055e62b278efd52d71
mkpoblet <mkpoblet@uio.no>
2024-03-23 23:22:55 +0100

    Add Logcat message to check amount of API-calls

--- project/app/src/main/java/no/uio/ifi/in2000/project/ui/home/HomeViewModel.kt
+++ project/app/src/main/java/no/uio/ifi/in2000/project/ui/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package no.uio.ifi.in2000.project.ui.home
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -91,8 +92,10 @@ class HomeViewModel : ViewModel(){
     fun loadData(lat: Double, lon: Double, county:String, lang: String){
         viewModelScope.launch(Dispatchers.IO){
             weatherData = locationForecastRep.fetchWeather(lat, lon)
+            Log.d("VIEWMODEL_HOMESCREEN", "API-kall weather") //sjekker antall API-kall vi gjør gjennom ViewModel. Vi fetcher ikke flere ganger, så det gir mening.
             locationForecastIcons = locationForecastRep.fetchLocationForecastIcons(weatherData)
             alertsData = metAlertsRep.fetchAlerts(county, lang)
+            Log.d("VIEWMODEL_HOMESCREEN", "API-kall alerts")
             sortedAlerts = metAlertsRep.sortAlerts(alertsData)
             metAlertsIcons = metAlertsRep.fetchAlertIcons(alertsData)
             responseStatus = true
