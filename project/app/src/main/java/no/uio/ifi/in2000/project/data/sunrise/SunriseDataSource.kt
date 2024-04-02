8d2bfb298745c2d4c7137d60fe584bddf7ab1131
ibenso <ibenso@uio.no>
2024-04-02 14:31:27 +0200

    Attempted to get current date, for parameter to sunrise API

--- project/app/src/main/java/no/uio/ifi/in2000/project/data/sunrise/SunriseDataSource.kt
+++ project/app/src/main/java/no/uio/ifi/in2000/project/data/sunrise/SunriseDataSource.kt
@@ -6,6 +6,8 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.serialization.gson.gson
 import no.uio.ifi.in2000.project.model.sunrise.SunriseResponse
+import java.text.SimpleDateFormat
+import java.util.Date
 
 data class SunriseDataSource(private val path: String  = "https://api.met.no/weatherapi/sunrise/3.0/") {
 
@@ -16,12 +18,19 @@ data class SunriseDataSource(private val path: String  = "https://api.met.no/wea
     }
 
     suspend fun getSunrise(lat: Double, lon: Double): SunriseResponse? {
+        val currentDate = getCurrentDate()
+
         return try {
-            val httpResponse = client.get("https://api.met.no/weatherapi/sunrise/3.0/sun?/$lat&lon=$lon")
+            val httpResponse = client.get("https://api.met.no/weatherapi/sunrise/3.0/sun?/$lat&lon=$lon&date=$currentDate")
             httpResponse.body<SunriseResponse>()
         } catch (e: Exception) {
             null
         }
+    }
 
+    private fun getCurrentDate(): String {
+        val currentDate = Date()
+        val formatter = SimpleDateFormat("yyyy-mm-dd")
+        return formatter.format(currentDate)
     }
 }
--- project/app/src/main/java/no/uio/ifi/in2000/project/data/sunrise/SunriseRepository.kt
+++ project/app/src/main/java/no/uio/ifi/in2000/project/data/sunrise/SunriseRepository.kt
@@ -1,4 +1,9 @@
 package no.uio.ifi.in2000.project.data.sunrise
 
-class SunriseRepository {
+class SunriseRepository() {
+    private val sunriseDataSource = SunriseDataSource()
+
+    suspend fun fetchSunrise(lat: Double, lon: Double) {
+        sunriseDataSource.getSunrise(lat, lon)
+    }
 }
