1692dd5b0e82530ac83094deaa725bb7eee0f484
mkpoblet <mkpoblet@uio.no>
2024-04-03 20:36:48 +0200

    Clean import in HomeScreen

--- project/app/src/main/java/no/uio/ifi/in2000/project/ui/home/HomeScreen.kt
+++ project/app/src/main/java/no/uio/ifi/in2000/project/ui/home/HomeScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
