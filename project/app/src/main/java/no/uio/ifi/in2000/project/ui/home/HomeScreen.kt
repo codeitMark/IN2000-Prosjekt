f7798595795547b15b1d86a6a4fba2289f5da921
johnrav <johnrav@ulrik.uio.no>
2024-04-04 14:15:19 +0200

    Added autocomplete textField

--- project/app/src/main/java/no/uio/ifi/in2000/project/ui/home/HomeScreen.kt
+++ project/app/src/main/java/no/uio/ifi/in2000/project/ui/home/HomeScreen.kt
@@ -1,5 +1,49 @@
 package no.uio.ifi.in2000.project.ui.home
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.KeyboardArrowDown
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
 import android.location.Address
 import android.location.Geocoder
 import android.util.Log
@@ -72,43 +116,10 @@ fun HomeScreen(vm: HomeViewModel = viewModel()) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "SEARCHBAR")
-        ExposedDropdownMenuBox(expanded = expandedBy,
-            onExpandedChange = { expandedBy = !expandedBy }) {
-            TextField(
-                modifier = Modifier
-                    .menuAnchor()
-                    .padding(top = 20.dp),
-                value = valgtOmråde,
-                onValueChange = {},
-                readOnly = true,
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expandedBy) },
-                label = { Text("Velg by!") })
 
-            ExposedDropdownMenu(expanded = expandedBy,
-                onDismissRequest = { expandedBy = false }) {
-                locations.forEach { by ->
-                    DropdownMenuItem(text = {Text(by)},
-                        onClick = {
-                            valgtOmråde = by
-                            expandedBy = false
-                            //Antar at vi er i Norge. Kom i Danmark med Strømmen... ???
-                            addressList = geocoder.getFromLocationName("$valgtOmråde, Norway", 1) //deprecated in API Level 33.
-                            if (addressList != null && addressList!!.isNotEmpty()){
-                                address = addressList?.get(0)
-                                vm.lat = address!!.latitude
-                                vm.lon = address!!.longitude
-                                vm.county = address!!.adminArea
-                                vm.lang = valgtSpråk
-                                vm.loadData(vm.lat, vm.lon, vm.county, vm.lang)
-                                Log.i("HOMESCREEN", "addressList: $addressList")
-                            } else{
-                                Log.w("HOMESCREEN", "addressList is null or empty! addressList: $addressList")
-                            }
-                        }
-                    )
-                }
-            }
-        }
+        AutoComplete()
+
+
         ExposedDropdownMenuBox(expanded = expandedSpråk,
             onExpandedChange = { expandedSpråk = !expandedSpråk }) {
             TextField(
@@ -248,3 +259,215 @@ fun HomeScreen(vm: HomeViewModel = viewModel()) {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AutoComplete() {
+
+    val animals = listOf(
+        "Lion",
+        "Tiger",
+        "Leopard",
+        "Cheetah",
+        "Giraffe",
+        "Elephant",
+        "Zebra",
+        "Kangaroo",
+        "Koala",
+        "Panda",
+        "Gorilla",
+        "Hippopotamus",
+        "Rhinoceros",
+        "Orangutan",
+        "Polar Bear",
+        "Grizzly Bear",
+        "Sloth",
+        "Kangaroo",
+        "Koala",
+        "Panda",
+        "Gorilla",
+        "Hippopotamus",
+        "Rhinoceros",
+        "Orangutan",
+        "Polar Bear",
+        "Grizzly Bear",
+        "Sloth",
+        "Kangaroo",
+        "Koala",
+        "Panda",
+        "Gorilla",
+        "Hippopotamus",
+        "Rhinoceros",
+        "Orangutan",
+        "Polar Bear",
+        "Grizzly Bear",
+        "Sloth",
+        "Kangaroo",
+        "Koala",
+        "Panda",
+        "Gorilla",
+        "Hippopotamus",
+        "Rhinoceros",
+        "Orangutan",
+        "Polar Bear",
+        "Grizzly Bear",
+        "Sloth"
+    )
+
+
+    var category by remember {
+        mutableStateOf("")
+    }
+
+    val heightTextFields by remember {
+        mutableStateOf(55.dp)
+    }
+
+    var textFieldSize by remember {
+        mutableStateOf(Size.Zero)
+    }
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    val interactionSource = remember {
+        MutableInteractionSource()
+    }
+
+    // Category Field
+    Column(
+        modifier = Modifier
+            .padding(30.dp)
+            .fillMaxWidth()
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = {
+                    expanded = false
+                }
+            )
+    ) {
+
+        Text(
+            modifier = Modifier.padding(start = 3.dp, bottom = 2.dp),
+            text = "Animals",
+            fontSize = 16.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.Medium
+        )
+
+        Column(modifier = Modifier.fillMaxWidth()) {
+
+            Row(modifier = Modifier.fillMaxWidth()) {
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(heightTextFields)
+                        .border(
+                            width = 1.8.dp,
+                            color = Color.Black,
+                            shape = RoundedCornerShape(15.dp)
+                        )
+                        .onGloballyPositioned { coordinates ->
+                            textFieldSize = coordinates.size.toSize()
+                        },
+                    value = category,
+                    onValueChange = {
+                        category = it
+                        expanded = true
+                    },
+                    placeholder = { Text("Enter any Animals Name") },
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        cursorColor = Color.Black
+                    ),
+                    textStyle = TextStyle(
+                        color = Color.Black,
+                        fontSize = 16.sp
+                    ),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Done
+                    ),
+                    singleLine = true,
+                    trailingIcon = {
+                        IconButton(onClick = { expanded = !expanded }) {
+                            Icon(
+                                modifier = Modifier.size(24.dp),
+                                imageVector = Icons.Rounded.KeyboardArrowDown,
+                                contentDescription = "arrow",
+                                tint = Color.Black
+                            )
+                        }
+                    }
+                )
+            }
+
+            AnimatedVisibility(visible = expanded) {
+                Card(
+                    modifier = Modifier
+                        .padding(horizontal = 5.dp)
+                        .width(textFieldSize.width.dp),
+                    shape = RoundedCornerShape(10.dp)
+                ) {
+
+                    LazyColumn(
+                        modifier = Modifier.heightIn(max = 150.dp),
+                    ) {
+
+                        if (category.isNotEmpty()) {
+                            items(
+                                animals.filter {
+                                    it.lowercase()
+                                        .contains(category.lowercase()) || it.lowercase()
+                                        .contains("others")
+                                }
+                                    .sorted()
+                            ) {
+                                ItemsCategory(title = it) { title ->
+                                    category = title
+                                    expanded = false
+                                }
+                            }
+                        } else {
+                            items(
+                                animals.sorted()
+                            ) {
+                                ItemsCategory(title = it) { title ->
+                                    category = title
+                                    expanded = false
+                                }
+                            }
+                        }
+
+                    }
+
+                }
+            }
+
+        }
+
+    }
+
+
+}
+
+@Composable
+fun ItemsCategory(
+    title: String,
+    onSelect: (String) -> Unit
+) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                onSelect(title)
+            }
+            .padding(10.dp)
+    ) {
+        Text(text = title, fontSize = 16.sp)
+    }
+
+}
